b61489b4458d1d3318a642b91892e1e840334562
Julio Muñoz <ing.juliomb@gmail.com>
2023-10-25 15:31:12 -0500

    Ejercicios typescript de los tipos básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,8 +1,38 @@
-console.log("Hello typescript");
+//Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number, b: number) {
-  return a + b;
+//Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+//String
+let nombre: string = "Julio";
+let saludo = `Me llamo ${nombre}`;
+
+//Arreglos
+// Se puede definir si es de algun tipo especifico, que sean diversos o que sea una combinación
+let people: string[] = [];
+people = ["Pedro", "Juan", "Milena"];
+people.push("100");
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push("Oliva");
+peopleAndNumbers.push(1001);
+
+//Enum
+//permiten definir un conjunto de constantes con nombre
+
+enum Color {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul",
 }
+let colorFavorito: Color = Color.Verde;
+console.log(`Mi color favorito es: ${colorFavorito}`);
 
-const sum = add(1, 2);
-console.log(sum);
+//Any
+let comodin: any = "Joker";
+comodin = { type: "Pepe" };
